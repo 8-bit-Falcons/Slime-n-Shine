fdc9ce3e6be289dd883237911006272910eefecd
Cierra <cierra362@gmail.com>
2024-06-10 00:15:59 -0400

    edited controls

--- core/assets/dialogue_balloon/balloon.gd
+++ core/assets/dialogue_balloon/balloon.gd
@@ -158,8 +158,9 @@ func _on_balloon_gui_input(event: InputEvent) -> void:
 	# See if we need to skip typing of the dialogue
 	if dialogue_label.is_typing:
 		var mouse_was_clicked: bool = event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed()
+		var next_button_was_pressed: bool = event.is_action_pressed(next_action)
 		var skip_button_was_pressed: bool = event.is_action_pressed(skip_action)
-		if mouse_was_clicked or skip_button_was_pressed:
+		if mouse_was_clicked or next_button_was_pressed or skip_button_was_pressed:
 			get_viewport().set_input_as_handled()
 			dialogue_label.skip_typing()
 			return
