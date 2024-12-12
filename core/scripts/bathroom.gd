b28b36894c6671eceb168a932cfe6d6a9eb1ba35
Cierra <cierra362@gmail.com>
2024-12-11 23:49:53 -0500

    all slimes turn to you during interactions you can also give any of them the markers

---
+++ core/scripts/bathroom.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+
+@onready var sticky: CharacterBody2D = $Sticky
+@onready var slorp: CharacterBody2D = $Slorp
+@onready var lint: CharacterBody2D = $Lint
+@onready var player: Player = $player
